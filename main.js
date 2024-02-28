c4959ab49b52fd6332f34cd04414afe6a4ce2ba8
Emilio Zapata <124ezapata@frhsd.com>
2024-02-28 07:44:25 -0500

    lesson 7 completed

--- main.js
+++ main.js
@@ -3,8 +3,34 @@ const app = Vue.createApp({
         return{
             product: 'socks',
             // Solution
-            description: 'A warm fuzzy pair of socks.' 
-             // Solution
+            description: 'A warm fuzzy pair of socks.' ,
+            image: './assets/images/socks_green.jpg',
+            url: "https://www.vuemastery.com",
+            inventory: 100,
+            onSale: true,
+            details: ["50 cotton", "30% wool", "20% polyester"],
+            variants:[
+                { id: 2234, color: 'green', image: "./assets/images/socks_green.jpg"},
+                { id: 2235, color: 'blue', image: "./assets/images/socks_blue.jpg"}
+            ],
+            sizes: ["xsmall", "small", "medium", "large", "xlarge"],
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1;
+        },
+        updateImage(variantImage){
+            this.image = variantImage;
+        },
+        removeFromCart(){
+            if (this.cart>0){
+                this.cart -=1;
+            } else{
+                this.cart = 0;
+            }
+           
         }
     }
 })
+
