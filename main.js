6d8be36abdc4e664f1bab7135b5f9ded8c8c10ec
Emilio Zapata <124ezapata@frhsd.com>
2024-02-29 09:49:09 -0500

    lesson 9 complete

--- main.js
+++ main.js
@@ -1,36 +1,26 @@
 const app = Vue.createApp({
     data() {
-        return{
-            product: 'socks',
-            // Solution
-            description: 'A warm fuzzy pair of socks.' ,
-
-            url: "https://www.vuemastery.com",
-
-            onSale: true,
-            details: ["50 cotton", "30% wool", "20% polyester"],
-            variants:[
-                { id: 2234, color: 'green', image: "./assets/images/socks_green.jpg"},
-                { id: 2235, color: 'blue', image: "./assets/images/socks_blue.jpg"}
+        return {
+            cart:0,
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ],
-            sizes: ["xsmall", "small", "medium", "large", "xlarge"],
+            // solution
+            onSale: true
+            // solution
         }
     },
-    methods:{
-        addToCart(){
-            this.cart +=1;
+    methods: {
+        addToCart() {
+            this.cart += 1
         },
-        updateVariant(index){
-            this.selectedVariant = index;
-
-        },
-        removeFromCart(){
-            if (this.cart>0){
-                this.cart -=1;
-            } else{
-                this.cart = 0;
-            }
-           
+        updateVariant(index) {
+            this.selectedVariant = index
         }
     },
         computed: {
