82dae5bb18938d232bdc23218a233e1bf0e75483
Emilio Zapata <124ezapata@frhsd.com>
2024-03-05 09:49:06 -0500

    finish

--- main.js
+++ main.js
@@ -1,45 +1,26 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ],
-            // solution
-            onSale: true
-            // solution
+    data () {
+        return{
+            cart:[],
+            premium: true,
+            productdetails: ["30% cotton", "60% wool", "20% polyester"],
         }
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
+    methods:{
+        updateCart(id){
+            this.cart.push(id)
         },
-        updateVariant(index) {
-            this.selectedVariant = index
-        }
-    },
-        computed: {
-            title() {
-                return this.brand + ' ' + this.product
-            },
-            image() {
-                return this.variants[this.selectedVariant].image
-            },
-            inStock() {
-                return this.variants[this.selectedVariant].quantity
-            },
-            // solution
-            sale() {
-                if (this.onSale) {
-                    return this.brand + ' ' + this.product + ' is on sale.'
-                }
-                return ''
+        removeCart(id){
+            if (this.cart.length === 0){
+                this.cart.length === 0;
+                
             }
+           
+             else{
+                this.cart.pop(id);   
+                
+            }
+        }
     }
 })
 
